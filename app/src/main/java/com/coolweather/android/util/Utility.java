df05cf6be2149a8dda9a63412ca3fb174f04ffe3
xsp <749854055@qq.com>
2018-06-06 09:44:14 +0800

    加入显示天气信息的功能。

--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -5,7 +5,7 @@ import android.text.TextUtils;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
-/*import com.coolweather.android.gson.Weather;*/
+import com.coolweather.android.gson.Weather;
 import com.google.gson.Gson;
 
 import org.json.JSONArray;
@@ -81,10 +81,9 @@ public class Utility {
         }
         return false;
     }
-}
-/*
 
-      将返回的JSON数据解析成Weather实体类
+
+
 
     public static Weather handleWeatherResponse(String response) {
         try {
@@ -97,5 +96,6 @@ public class Utility {
         }
         return null;
     }
-*/
 
+
+}
